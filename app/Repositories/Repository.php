0104e6effaf4099268d4ae82f1e687d2847da570
Riadh Benamara <113543372+Riadbenam@users.noreply.github.com>
2023-02-16 17:01:23 +0100

    Mdp generate

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -3,12 +3,37 @@
 namespace App\Repositories;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Str;
 
 class Repository {
     function createDatabase(): void {
         DB::unprepared(file_get_contents('database/build.sql'));
+    }
+
+    function insertTeacher(array $teacher): void {
+        if(!array_key_exists('IdProf', $teacher)){
+            $id = dechex(time());
+            $id = substr($id, 3, 10);
+            $teacher['IdProf'] = "PRF".$id;
+        }
+        DB::table('Enseignants')
+            ->insert($teacher);
+    }
+
+    function teachers() : array{
+        return DB::table('Enseignants')
+                    ->get()
+                    ->toArray();
+    }
+
+    function getTeacher(string $name, string $firstname) : array{
+        $teacher = DB::table('Enseignants')
+                    ->where('nomProf', $name)
+                    ->where('firstname', $firstname)
+                    ->get()
+                    ->toArray();
+        if(empty($teacher))
+            throw new Exception('Enseignant inconnu'); 
+        return $teacher[0];
     }
     public function updateTeacher($username)
     {
@@ -24,10 +49,10 @@ class Repository {
             $user->save();
             return $password;
         }
-    
+
         return null;
     }
-    
+
     private function createPassword($IdProf,$length)
     {
         $password = Str::random($length);
